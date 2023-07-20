aad50954736e69e18db60ef172341955baae2acb
Likhitha <likhithathammireddy@gmail.com>
2023-07-20 12:10:20 +0530

    updated code

--- Jenkins_demo/src/test/java/in/amazon/Jenkins_demo/VerifyTitleTest.java
+++ Jenkins_demo/src/test/java/in/amazon/Jenkins_demo/VerifyTitleTest.java
@@ -12,17 +12,7 @@ public class VerifyTitleTest {
 	@BeforeTest
 	public void launchApplication() {
 	driver= new ChromeDriver();
-	String os = System.getProperty("os.name").toLowerCase();
-	if (os.contains("win")){			
-		driver = new ChromeDriver();
-	}
-	    
-	else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless=new");			
-		driver = new ChromeDriver(options);
-	}
-
+	
 
 	driver .get("https://facebook.com");
 	}
